3382eb77077b028d83848d7bd69eeaf219deeadf
Rain Alert <jacajeras@gmail.com>
2023-01-31 23:47:56 +0800

    Added current year variable on all routes.

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from datetime import date
 #Create json in https://www.npoint.io/
 posts = requests.get(url="https://api.npoint.io/dd906d71fd00e2528fde").json()
 
+current_year = date.today().year
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    current_year = date.today().year
     return render_template("index.html", all_posts=posts, year=current_year)
 
 @app.route("/post/<int:id_num>")
@@ -18,11 +19,11 @@ def get_post(id_num):
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", year=current_year)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", year=current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
