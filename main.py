644f41f2492c965dba278f7b8577eae8b18e9190
Rain Alert <jacajeras@gmail.com>
2023-02-01 22:53:29 +0800

    Made contact.html to work using smtplib.

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from datetime import date
+import smtplib
+import os
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:Upgraded Blog\n\nName: {name}\nEmail: {email}\n Phone:{phone}\nMessage: {message}"
+    with smtplib.SMTP("smtp.office365.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=receiving_email,
+                            msg=email_message)
 
 #Create json in https://www.npoint.io/
 posts = requests.get(url="https://api.npoint.io/dd906d71fd00e2528fde").json()
 
 current_year = date.today().year
+my_email = os.environ["EMAIL"]
+my_password = os.environ["PASSWORD"]
+receiving_email = os.environ["RECEIVER"]
 
 app = Flask(__name__)
 
@@ -21,8 +35,12 @@ def get_post(id_num):
 def about():
     return render_template("about.html", year=current_year)
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        data = request.form
+        send_email(name=data["name"], email=data["email"], phone=data["phone"], message=data["message"])
+        return render_template("contact.html", year=current_year)
     return render_template("contact.html", year=current_year)
 
 if __name__ == "__main__":
